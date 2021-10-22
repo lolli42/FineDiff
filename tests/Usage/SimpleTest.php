c8e6e8027f742616a074d4617b8a0dadcefb5b76
Christian Kuhn <lolli@schwarzbu.ch>
2021-10-22 02:34:28 +0200

    [TASK] Inline test fixtures to SimpleTest
    And refactor to use a data provider.

--- tests/Usage/SimpleTest.php
+++ tests/Usage/SimpleTest.php
@@ -4,6 +4,7 @@ namespace cogpowered\FineDiff\Tests\Usage;
 
 use cogpowered\FineDiff\Diff;
 use cogpowered\FineDiff\Granularity\Character;
+use cogpowered\FineDiff\Granularity\GranularityInterface;
 use cogpowered\FineDiff\Granularity\Paragraph;
 use cogpowered\FineDiff\Granularity\Sentence;
 use cogpowered\FineDiff\Granularity\Word;
@@ -13,107 +14,58 @@ use PHPUnit\Framework\TestCase;
 
 class SimpleTest extends TestCase
 {
+    public function processAndRenderDataProvider()
+    {
+        return [
+            'character' => [
+                new Character(),
+                'c28dc12di:sc73',
+                'This is the 1st sentence. It<del>s</del> then carrie<del>d</del><ins>s</ins> on into another.'
+                    . chr(10) . 'This is another paragraph, just to test things further!'
+            ],
+            'word' => [
+                new Word(),
+                'c26d4i3:It c5d8i8:carries c72',
+                'This is the 1st sentence. <del>Its </del><ins>It </ins>then <del>carried </del><ins>carries </ins>on into another.'
+                    . chr(10) . 'This is another paragraph, just to test things further!'
+            ],
+            'paragraph' => [
+                new Paragraph(),
+                'd60i59:This is the 1st sentence. It then carries on into another.'
+                    . chr(10) . 'c55',
+                '<del>This is the 1st sentence. Its then carried on into another.'
+                    . chr(10) . '</del><ins>This is the 1st sentence. It then carries on into another.'
+                    . chr(10) . '</ins>This is another paragraph, just to test things further!'
+            ],
+            'sentence' => [
+                new Sentence(),
+                'c25d35i34: It then carries on into another.'
+                    . chr(10) . 'c55',
+                'This is the 1st sentence.<del> Its then carried on into another.\n</del><ins> It then carries on into another.'
+                    . chr(10) . '</ins>This is another paragraph, just to test things further!'
+            ],
+        ];
+    }
+
     /**
-     * Helper method to retrieve fixture file content.
+     * @dataProvider processAndRenderDataProvider
+     * @test
      */
-    protected function getFile($file)
+    public function parseAndRender(GranularityInterface $granularity, string $expectedOpcode, string $expectedHtml)
     {
-        $txt = file_get_contents(__DIR__ . '/Resources/' . $file . '.txt');
-        $txt = explode('==========', $txt);
+        $from = 'This is the 1st sentence. Its then carried on into another.'
+            . chr(10) . 'This is another paragraph, just to test things further!';
+        $to = 'This is the 1st sentence. It then carries on into another.'
+            . chr(10) . 'This is another paragraph, just to test things further!';
 
-        $from    = trim($txt[0]);
-        $to      = trim($txt[1]);
-        $opcodes = trim($txt[2]);
-        $html    = trim($txt[3]);
-
-        return [$from, $to, $opcodes, $html];
-    }
-
-    public function testInsertCharacterGranularity()
-    {
-        list($from, $to, $opcodes, $html) = $this->getFile('character/simple');
-
-        $diff = new Diff(new Character());
-        $generated_opcodes = $diff->getOpcodes($from, $to);
-
-        // Generate opcodes
-        self::assertEquals($generated_opcodes, $opcodes);
-
-        // Render to text from opcodes
+        $diff = new Diff($granularity);
+        $generated_opcodes = (string)$diff->getOpcodes($from, $to);
+        self::assertEquals($generated_opcodes, $expectedOpcode);
         $render = new Text();
-        self::assertEquals($render->process($from, $generated_opcodes), $to);
-
-        // Render to html from opcodes
+        self::assertEquals((string)$render->process($from, $generated_opcodes), $to);
+        self::assertEquals($diff->render($from, $to), $expectedHtml);
         $render = new Html();
-        self::assertEquals($render->process($from, $generated_opcodes), $html);
-
-        // Render
-        self::assertEquals($diff->render($from, $to), $html);
-    }
-
-    public function testInsertWordGranularity()
-    {
-        list($from, $to, $opcodes, $html) = $this->getFile('word/simple');
-
-        $diff = new Diff(new Word());
-        $generated_opcodes = $diff->getOpcodes($from, $to);
-
-        // Generate opcodes
-        self::assertEquals($generated_opcodes, $opcodes);
-
-        // Render to text from opcodes
-        $render = new Text();
-        self::assertEquals($render->process($from, $generated_opcodes), $to);
-
-        // Render to html from opcodes
-        $render = new Html();
-        self::assertEquals($render->process($from, $generated_opcodes), $html);
-
-        // Render
-        self::assertEquals($diff->render($from, $to), $html);
-    }
-
-    public function testInsertSentenceGranularity()
-    {
-        list($from, $to, $opcodes, $html) = $this->getFile('sentence/simple');
-
-        $diff = new Diff(new Sentence());
-        $generated_opcodes = $diff->getOpcodes($from, $to);
-
-        // Generate opcodes
-        self::assertEquals($generated_opcodes, $opcodes);
-
-        // Render to text from opcodes
-        $render = new Text();
-        self::assertEquals($render->process($from, $generated_opcodes), $to);
-
-        // Render to html from opcodes
-        $render = new Html();
-        self::assertEquals($render->process($from, $generated_opcodes), $html);
-
-        // Render
-        self::assertEquals($diff->render($from, $to), $html);
-    }
-
-    public function testInsertParagraphGranularity()
-    {
-        list($from, $to, $opcodes, $html) = $this->getFile('paragraph/simple');
-
-        $diff = new Diff(new Paragraph());
-        $generated_opcodes = $diff->getOpcodes($from, $to);
-
-        // Generate opcodes
-        self::assertEquals($generated_opcodes, $opcodes);
-
-        // Render to text from opcodes
-        $render = new Text();
-        self::assertEquals($render->process($from, $generated_opcodes), $to);
-
-        // Render to html from opcodes
-        $render = new Html();
-        self::assertEquals($render->process($from, $generated_opcodes), $html);
-
-        // Render
-        self::assertEquals($diff->render($from, $to), $html);
+        self::assertEquals($render->process($from, $generated_opcodes), $expectedHtml);
+        self::assertEquals($diff->render($from, $to), $expectedHtml);
     }
 }
