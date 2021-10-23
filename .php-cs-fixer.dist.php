b3e3fc78fbd9b114ca8950b282738e1c656f28e3
Christian Kuhn <lolli@schwarzbu.ch>
2021-10-23 14:43:56 +0200

    [TASK] Update PHP file header

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -24,6 +24,14 @@ return (new PhpCsFixer\Config())
         'declare_strict_types' => true,
         'dir_constant' => true,
         'function_typehint_space' => true,
+        'header_comment' => [
+            'header' => 'FINE granularity DIFF'
+                . chr(10) . chr(10) . '(c) 2011 Raymond Hill (http://raymondhill.net/blog/?p=441)'
+                . chr(10) . '(c) 2013 Robert Crowe (http://cogpowered.com)'
+                . chr(10) . '(c) 2021 Christian Kuhn'
+                . chr(10). chr(10) . 'For the full copyright and license information, please view'
+                . chr(10) . 'the LICENSE file that was distributed with this source code.',
+        ],
         'lowercase_cast' => true,
         'method_argument_space' => ['on_multiline' => 'ensure_fully_multiline'],
         'modernize_types_casting' => true,
