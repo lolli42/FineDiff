b25f27acd00e05ff9e34226b57df025a55e33bb0
Christian Kuhn <lolli@schwarzbu.ch>
2021-10-23 14:43:56 +0200

    [TASK] Update PHP file header

--- src/Delimiters.php
+++ src/Delimiters.php
@@ -2,20 +2,15 @@
 
 declare(strict_types=1);
 
-/**
+/*
  * FINE granularity DIFF
  *
- * Computes a set of instructions to convert the content of
- * one string into another.
+ * (c) 2011 Raymond Hill (http://raymondhill.net/blog/?p=441)
+ * (c) 2013 Robert Crowe (http://cogpowered.com)
+ * (c) 2021 Christian Kuhn
  *
- * Originally created by Raymond Hill (https://github.com/gorhill/PHP-FineDiff), brought up
- * to date by Cog Powered (https://github.com/cogpowered/FineDiff).
- *
- * @copyright Copyright 2011 (c) Raymond Hill (http://raymondhill.net/blog/?p=441)
- * @copyright Copyright 2013 (c) Robert Crowe (http://cogpowered.com)
- * @link https://github.com/cogpowered/FineDiff
- * @version 0.0.1
- * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
  */
 
 namespace cogpowered\FineDiff;
--- src/Exceptions/GranularityCountException.php
+++ src/Exceptions/GranularityCountException.php
@@ -2,21 +2,16 @@
 
 declare(strict_types=1);
 
-/**
-* FINE granularity DIFF
-*
-* Computes a set of instructions to convert the content of
-* one string into another.
-*
-* Originally created by Raymond Hill (github.com/gorhill/PHP-FineDiff), brought up
-* to date by Cog Powered (github.com/cogpowered/FineDiff).
-*
-* @copyright Copyright 2011 (c) Raymond Hill (http://raymondhill.net/blog/?p=441)
-* @copyright Copyright 2013 (c) Robert Crowe (http://cogpowered.com)
-* @link https://github.com/cogpowered/FineDiff
-* @version 0.0.1
-* @license MIT License (http://www.opensource.org/licenses/mit-license.php)
-*/
+/*
+ * FINE granularity DIFF
+ *
+ * (c) 2011 Raymond Hill (http://raymondhill.net/blog/?p=441)
+ * (c) 2013 Robert Crowe (http://cogpowered.com)
+ * (c) 2021 Christian Kuhn
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
 
 namespace cogpowered\FineDiff\Exceptions;
 
